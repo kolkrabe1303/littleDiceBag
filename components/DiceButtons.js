2e8339aa15b80b95623300fca6f10187a2cbd9c0
Markus Neeb <kontakt@markusneeb.de>
2021-10-19 11:32:34 +0200

    Cleaned up die-rolling code

--- components/DiceButtons.js
+++ components/DiceButtons.js
@@ -7,6 +7,7 @@ app.component('dice-buttons', {
     <div class="dice-buttons">
         <table>
             <tr><td><input id="dieCount" v-model="diceCount"><button @click="setDiceCount(1)">1</button><button @click="setDiceCount(2)">2</button><button @click="setDiceCount(3)">3</button></td></tr>
+            <tr><td><hr></td></tr>
             <tr><td><button @click="rollDie(4)">d4</button></td></tr>
             <tr><td><button @click="rollDie(6)">d6</button></td></tr>
             <tr><td><button @click="rollDie(8)">d8</button></td></tr>
@@ -15,13 +16,18 @@ app.component('dice-buttons', {
             <tr><td><button @click="rollDie(20)">d20</button></td></tr>
             <tr><td><button @click="rollDie(100)">d100</button></td></tr>
             <tr><td><form class="button-form" @submit.prevent="rollCustomDie">d<input id="customDie" v-model="customDie"><input class="button" type="submit" value="Roll!"></form></td></tr>
+            <tr><td><hr></td></tr>
+            <tr><td>Per die bonus <input id="perDiceBonus" v-model="perDiceBonus"></td></tr>
+            <tr><td>Global bonus <input id="globalBonus" v-model="globalBonus"></td></tr>
         </table>
     </div>
     `,
     data() {
         return {
             customDie: null,
-            diceCount: 1
+            diceCount: 1,
+            perDieBonus: null,
+            globalBonus: null,
         }
     },
     methods: {
@@ -32,7 +38,7 @@ app.component('dice-buttons', {
             let resultValue = 0
             for (let i = 0; i<this.diceCount; i++) {
                 singleRolls.push(this.randomIntFromInterval(1, dieType))   
-                resultValue += singleRolls[i]
+                resultValue += singleRolls[i] + this.perDieBonus
             }
 
             //prepare result string for multiple dice
@@ -45,6 +51,8 @@ app.component('dice-buttons', {
                 }
                 singleRollsDetailsString += ")"
             }
+        
+            //Prepare result struct
             let result = {
                 value: resultValue,
                 dieType: dieType,
@@ -52,37 +60,13 @@ app.component('dice-buttons', {
                 singleRolls: singleRolls,
                 singleRollsDetailsString:  singleRollsDetailsString
             }
-            this.emitRollResult(result)
-        },
-        rollCustomDie() {
-            const singleRolls = []
-            let resultValue = 0
-            for (let i = 0; i<this.diceCount; i++) {
-                singleRolls.push(this.randomIntFromInterval(1, this.customDie))
-                resultValue += singleRolls[singleRolls.length - 1]
-            }
-            //prepare result string for multiple dice
-            let singleRollsDetailsString = ""
-            if (this.diceCount > 1) {
-                singleRollsDetailsString += "("
-                for (let i=0; i<this.diceCount; i++) {
-                    if (i > 0) singleRollsDetailsString += ", "
-                    singleRollsDetailsString += singleRolls[i]
-                }
-                singleRollsDetailsString += ")"
-            }
-            let result = {
-                value: resultValue,
-                dieType: this.customDie,
-                diceCount: this.diceCount,
-                singleRolls: singleRolls,
-                singleRollsDetailsString:  singleRollsDetailsString
-            }
-            this.emitRollResult(result)
-        },
-        emitRollResult(result) {
+
+            //Log event and emit result to main.js
             console.log("Rolled " + result.diceCount + " die of type: " + result.dieType + " Result: " + result.value)
             this.$emit('add-to-results', result)
+        },
+        rollCustomDie() {
+            this.rollDie(this.customDie)
         },
         randomIntFromInterval(min, max) {
             return Math.floor(Math.random() * (max - min + 1) + min)
