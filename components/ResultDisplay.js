69d5cb9891d2b6683925b06ed35e9fe5de9b5028
Markus Neeb <kontakt@markusneeb.de>
2021-10-20 20:29:57 +0200

    Full height design and scrollable results view

--- components/ResultDisplay.js
+++ components/ResultDisplay.js
@@ -8,8 +8,8 @@ app.component('result-display', {
     template: 
     /*html*/
     `
-    <h2>Results</h2>
-    <div class="result-display">
+    <div class="result-display mb-4">
+        <h2>Results</h2>
         <p class="border-bottom pb-1" v-for="result in results"> <b>{{result.diceCount}}d{{ result.dieType }}</b>: <span style="font-size: 1.5rem;">{{ result.value }}</span> <br><small>{{ result.singleRollsDetailsString }}</small></p>
     </div>
     `
