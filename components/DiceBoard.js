4087f13337973b530cf72d99c1eeaac802f2dbf7
Markus Neeb <kontakt@markusneeb.de>
2021-10-25 17:33:45 +0200

    Changed layout for output box

--- components/DiceBoard.js
+++ components/DiceBoard.js
@@ -3,30 +3,30 @@ app.component('dice-board', {
     template:
     /*html*/
     `
-    <div class="dice-board pb-2 mb-2 border-bottom">
+    <div class="dice-board pb-2 mb-2">
         <div class="dice-count mb-1 row">
-            <div class="col-6 text-center"><label class="no-wrap">Number of dice</label><br><div class="no-wrap w-100"><button @click="changeDiceCount(-1)" class="btn-pls-mns w-25">-</button><input id="dieCount" v-model="diceCount" class="w-50"><button @click="changeDiceCount(1)" class="btn-pls-mns w-25">+</button></div></div>
-            <div class="col-6 text-center"><label class="no-wrap">Per die bonus</label><br><div class="no-wrap w-100"><button @click="changePerDieBonus(-1)" class="btn-pls-mns w-25">-</button><input id="perDieBonus" v-model="perDieBonus" class="w-50"><button @click="changePerDieBonus(1)" class="btn-pls-mns w-25">+</button></div></div>
+            <div class="col-6 text-center"><label class="no-wrap">Number of dice</label><br><div class="no-wrap w-100 d-flex align-items-center"><div @click="changeDiceCount(-1)" class="btn-pls-mns w-25 btn btn-sm btn-secondary">-</div><input id="dieCount" v-model="diceCount" class="w-50 form-control"><div @click="changeDiceCount(1)" class="btn-pls-mns w-25 btn btn-sm btn-secondary">+</div></div></div>
+            <div class="col-6 text-center"><label class="no-wrap">Per die bonus</label><br><div class="no-wrap w-100 d-flex align-items-center"><div @click="changePerDieBonus(-1)" class="btn-pls-mns w-25 btn btn-sm btn-secondary">-</div><input id="perDieBonus" v-model="perDieBonus" class="w-50 form-control"><div @click="changePerDieBonus(1)" class="btn-pls-mns w-25 btn btn-sm btn-secondary">+</div></div></div>
         </div>
         <div class="dice-count pb-2 mb-2 row">
             <div class="col-6"></div>        
-            <div class="col-6 text-center"><label class="no-wrap">Global bonus</label><br><div class="no-wrap w-100"><button @click="changeGlobalBonus(-1)" class="btn-pls-mns w-25">-</button><input id="globalBonus" v-model="globalBonus" class="w-50"><button @click="changeGlobalBonus(1)" class="btn-pls-mns w-25">+</button></div></div>
+            <div class="col-6 text-center"><label class="no-wrap">Global bonus</label><br><div class="no-wrap w-100 d-flex align-items-center"><div @click="changeGlobalBonus(-1)" class="btn-pls-mns w-25 btn btn-sm btn-secondary">-</div><input id="globalBonus" v-model="globalBonus" class="w-50 form-control"><div @click="changeGlobalBonus(1)" class="btn-pls-mns w-25 btn btn-sm btn-secondary">+</div></div></div>
         </div>
         <div class="dice-buttons">
             <div class="row mb-2">
-                <div class="col-4 text-center"><button @click="rollDie(4)" class="w-100">d4</button></div>
-                <div class="col-4 text-center"><button @click="rollDie(6)" class="w-100">d6</button></div>
-                <div class="col-4 text-center"><button @click="rollDie(8)" class="w-100">d8</button></div>
+                <div class="col-4 text-center"><div @click="rollDie(4)" class="w-100 btn btn-secondary">d4</div></div>
+                <div class="col-4 text-center"><div @click="rollDie(6)" class="w-100 btn btn-secondary ">d6</div></div>
+                <div class="col-4 text-center"><div @click="rollDie(8)" class="w-100 btn btn-secondary">d8</div></div>
             </div>
             <div class="row mb-2">
-                <div class="col-4 text-center"><button @click="rollDie(10)" class="w-100">d10</button></div>
-                <div class="col-4 text-center"><button @click="rollDie(12)" class="w-100">d12</button></div>
-                <div class="col-4 text-center"><button @click="rollDie(20)" class="w-100">d20</button></div>
+                <div class="col-4 text-center"><div @click="rollDie(10)" class="w-100 btn btn-secondary">d10</div></div>
+                <div class="col-4 text-center"><div @click="rollDie(12)" class="w-100 btn btn-secondary">d12</div></div>
+                <div class="col-4 text-center"><div @click="rollDie(20)" class="w-100 btn btn-secondary">d20</div></div>
             </div>
             <div class="row mb-2">
-                <div class="col-4 text-center"><button @click="rollDie(100)" class="w-100">d100</button></div>
-                <div class="col-4 text-center"><button @click="rollCustomDie()" class="w-50">d</button><input id="customDie" v-model="customDie" class="text-start w-50" placeholder="xx"></div>
-                <div class="col-4 text-center"><button @click="clearBoard()" class="w-100">Clear</button></div>
+                <div class="col-4 text-center"><div @click="rollDie(100)" class="w-100 btn btn-secondary">d100</div></div>
+                <div class="col-4 text-center d-flex align-items-center"><div @click="rollCustomDie()" class="w-50 btn btn-secondary">d</div><input id="customDie" v-model="customDie" class="text-start w-50 form-control" placeholder="x"></div>
+                <div class="col-4 text-center"><div @click="clearBoard()" class="w-100 btn btn-secondary">Clear</div></div>
             </div>
         </div>
         
@@ -79,7 +79,9 @@ app.component('dice-board', {
                 dieType: dieType,
                 diceCount: this.diceCount,
                 singleRolls: singleRolls,
-                singleRollsDetailsString:  singleRollsDetailsString
+                singleRollsDetailsString:  singleRollsDetailsString,
+                perDieBonus: this.perDieBonus,
+                globalBonus: this.globalBonus
             }
 
             //Log event and emit result to main.js
--- components/ResultDisplay.js
+++ components/ResultDisplay.js
@@ -8,12 +8,26 @@ app.component('result-display', {
     template: 
     /*html*/
     `
-    <div class="result-display mb-2 ps-2">
+    <div class="result-display mb-2 p-2 bg-gray">
         <div v-for="result in results" class="mb-1">
-            <div class="d-inline-block w-25 pe-2 text-end"><b>{{result.diceCount}}d{{ result.dieType }}</b>:</div>
-            <div class="d-inline-block w-75" style="font-size: 1.5rem;">{{ result.value }}</div>
-            <div class="d-inline-block small" v-bind:class="{ ms25: result.singleRollsDetailsString }">{{ result.singleRollsDetailsString }}</div>
+            <div class="row">
+                <div class="result-roll-label col-sm-5 col-6 pe-2 d-flex align-items-end justify-content-end">
+                    <div class="fw-bold">
+                        {{result.diceCount}}d{{ result.dieType }}<span v-if="result.perDieBonus > 0">+{{ result.perDieBonus }}</span><span v-else-if="result.perDieBonus < 0">-{{ (result.perDieBonus * -1) }}</span>
+                        <span v-if="result.globalBonus > 0"> + {{ result.globalBonus }}</span>
+                        <span v-else-if="result.globalBonus < 0"> - {{ (result.globalBonus * -1) }}</span>
+                    </div>
+                    :
+                </div>
+                <div class="result-sum-label col-sm-7 col-6 ps-0 d-flex align-items-end justify-content-start"><span>{{ result.value }}</span></div>
+                <div class="row" v-if="result.singleRollsDetailsString">
+                    <div class="col-sm-5 col-6"></div>
+                    <div class="small result-math-label col-sm-7 col-6 d-flex align-items-end justify-content-start" v-bind:class="">{{ result.singleRollsDetailsString }}</div>
+                </div>
+            </div>
         </div>
     </div>
+    <!--{ ms35: result.singleRollsDetailsString }-->
     `
+
 })
