d5fa70184ad8125a2260ed1c0bb46f7ebd300415
Markus Neeb <kontakt@markusneeb.de>
2021-10-28 12:47:41 +0200

    Refactorized the result layout as a table

--- components/ResultDisplay.js
+++ components/ResultDisplay.js
@@ -8,17 +8,26 @@ app.component('result-display', {
     template: 
     /*html*/
     `
-    <div class="result-display mb-2 p-2 bg-gray">
-        <div v-for="result in results" class="mb-1">
-            <div class="result-roll-label d-inline-block text-end pe-2">
-                <strong>
-                    {{result.diceCount}}d{{ result.dieType }}<span v-if="result.perDieBonus > 0">+{{ result.perDieBonus }}</span><span v-else-if="result.perDieBonus < 0">-{{ (result.perDieBonus * -1) }}</span>
-                    <span v-if="result.globalBonus > 0"> + {{ result.globalBonus }}</span>
-                    <span v-else-if="result.globalBonus < 0"> - {{ (result.globalBonus * -1) }}</span>
-                </strong>:
-            </div>
-            <div class="result-sum-label fs-2 d-inline-block">{{ result.value }}</div>
-            <div class="result-math-label small" v-if="result.singleRollsDetailsString">{{ result.singleRollsDetailsString }}</div>
+    <div class="result-display mb-2 px-2 pb-1 pt-3 bg-gray">
+        <div v-for="result in results" class="result">
+            <table class="result-table">
+                <tr class="tr-result">
+                    <td class="td-roll">
+                        <strong>
+                            {{result.diceCount}}d{{ result.dieType }}<span v-if="result.perDieBonus > 0">+{{ result.perDieBonus * result.diceCount}}</span><span v-else-if="result.perDieBonus < 0">-{{ (result.perDieBonus * -1 * result.diceCount) }}</span>
+                            <span v-if="result.globalBonus > 0"> + {{ result.globalBonus }}</span>
+                            <span v-else-if="result.globalBonus < 0"> - {{ (result.globalBonus * -1) }}</span>
+                        </strong>:
+                    </td>
+                    <td class="td-sum">
+                        {{ result.value }}
+                    </td>
+                </tr>
+                <tr class="tr-math" v-if="result.singleRollsDetailsString">
+                    <td></td>
+                    <td class="td-math">{{ result.singleRollsDetailsString }}</td>
+                </tr>
+            </table>
         </div>
     </div>
     `
